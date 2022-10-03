c8054d0a83cd0ead6195a262a780928e252dd57b
beaulazear <beaulazear@gmail.com>
2022-10-03 17:57:47 -0400

    commit tree efd757239b901ad98196378de9476124b000592d

--- backup.js
+++ backup.js
@@ -2,8 +2,6 @@ function superbowlWin(array) {
     let yearWon = array.find(function(array){
         return array.result === `W`
     })
-    if(yearWon === undefined){
-        return yearWon;
-    }
     return yearWon.year;
 }
+
